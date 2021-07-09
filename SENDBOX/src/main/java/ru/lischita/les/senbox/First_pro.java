ee974652741e1212ce20833eb60ecb332d08d044
lischita <87008989+lischita@users.noreply.github.com>
2021-07-09 10:51:04 +0300

    Изменение настройки форматирования

--- SENDBOX/src/main/java/ru/lischita/les/senbox/First_pro.java
+++ SENDBOX/src/main/java/ru/lischita/les/senbox/First_pro.java
@@ -1,9 +1,8 @@
 package ru.lischita.les.senbox;
 
-public class First_pro
-{
-   public static void main(String[] args)
-    {
-      System.out.println("Hello world");
-     }
+public class First_pro {
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world");
+  }
 }
