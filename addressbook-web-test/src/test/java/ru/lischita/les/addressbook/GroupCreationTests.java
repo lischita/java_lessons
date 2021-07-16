7587a54cb57b3a79bbbb5dc8734b181badc171c6
lischita <87008989+lischita@users.noreply.github.com>
2021-07-16 16:28:31 +0300

    Перенес из метода testGroupCreation () в метод  setUp()  действия связанные с вводом логина и пароля на странице

--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/GroupCreationTests.java
@@ -16,10 +16,6 @@ public class GroupCreationTests {
     wd = new ChromeDriver();
     //driver = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -27,6 +23,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.id("LoginForm")).submit();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
