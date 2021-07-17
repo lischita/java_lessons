b56109b5141ceec54c4c1f6ec581f0c3b21d1cc6
lischita <87008989+lischita@users.noreply.github.com>
2021-07-17 11:49:00 +0300

    Формирование специализированных классов помощников и группировка их в общий базовый класс помощников

---
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,39 @@
+package ru.lischita.les.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.lischita.les.addressbook.model.GroupData;
+
+public class GroupHelper extends HelperBase{
+  public GroupHelper(WebDriver wd) {
+    super(wd);
+  }
+
+  public void returntoGroupPage() {
+    click(By.linkText("group page"));
+  }
+
+  public void submitGroupCreation() {
+    click(By.name("submit"));
+  }
+
+  public void fillGroupForm(GroupData groupData) {
+    type(By.name("group_name"), groupData.getName());
+    click(By.name("group_header"));
+    click(By.xpath("//form[@action='/addressbook/group.php']"));
+    type(By.name("group_header"), groupData.getHeader());
+    type(By.name("group_footer"), groupData.getFooter());
+  }
+
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
+
+  public void deletedSelectedGroups() {
+    click(By.name("delete"));
+  }
+
+  public void selectGroup() {
+    click(By.name("selected[]"));
+  }
+}
---
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/GroupDeletionTest.java
@@ -0,0 +1,15 @@
+package ru.lischita.les.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deletedSelectedGroups();
+    app.getGroupHelper().returntoGroupPage();
+  }
+
+}
