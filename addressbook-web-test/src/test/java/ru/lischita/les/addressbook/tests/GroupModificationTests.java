4e7e420babd4b8fb454491dc6598e116a8590bf2
lischita <87008989+lischita@users.noreply.github.com>
2021-07-19 10:13:17 +0300

    Добавлен тест GroupModificationTests

---
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.lischita.les.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.lischita.les.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification ()
+  {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returntoGroupPage();
+
+  }
+
+}
