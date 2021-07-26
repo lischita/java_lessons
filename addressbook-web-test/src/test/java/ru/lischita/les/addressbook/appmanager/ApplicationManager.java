ac888b943c440d381514f42cdc727638f74206d7
lischita <87008989+lischita@users.noreply.github.com>
2021-07-26 14:21:24 +0300

    Задание №7 Тесты исполняются в FireFox

--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/appmanager/ApplicationManager.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import java.util.concurrent.TimeUnit;
@@ -26,14 +28,19 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if(browser.equals(BrowserType.CHROME)) { wd = new ChromeDriver();}
-    else if(browser.equals(BrowserType.IE)) {
-     System.setProperty("webdriver.ie.driver", "C:\\Users\\Елизавета Криворучка\\Desktop\\world\\IEDriverServer.exe");
-       wd = new InternetExplorerDriver();
-       wd.manage().window().maximize();
+    if(browser.equals(BrowserType.CHROME)) {
+      System.setProperty("webdriver.chrome.driver", "C:\\Users\\Елизавета Криворучка\\Desktop\\world\\chromedriver.exe");
+      wd = new ChromeDriver();
     }
-    //driver = new FirefoxDriver();
-   // wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    else if(browser.equals(BrowserType.IE)) {
+      System.setProperty("webdriver.ie.driver", "C:\\Users\\Елизавета Криворучка\\Desktop\\world\\IEDriverServer.exe");
+       wd = new InternetExplorerDriver();
+      // wd.manage().window().maximize();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      System.setProperty("webdriver.gecko.driver", "C:\\Users\\Елизавета Криворучка\\Desktop\\world\\geckodriver.exe");
+      wd = new FirefoxDriver();
+    }
+   //wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
