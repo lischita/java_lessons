5416b15572d341e1b2c8d048d10b5adfe664468a
lischita <87008989+lischita@users.noreply.github.com>
2021-07-31 14:51:43 +0300

    Сортировка списков при Сравнении тестах  GroupCreationTests и GroupModificationTests

--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/model/GroupData.java
@@ -9,8 +9,10 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+
+
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -45,18 +47,17 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    return Objects.hash(name);
   }
 
 }
--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/GroupCreationTests.java
@@ -24,10 +24,16 @@ public class GroupCreationTests extends TestBase {
     for (GroupData g:after){
       if(g.getId()>max){max=g.getId();}
     }
-    group.setId(max);*/
+    group.setId(max);
     group.setId(after.stream().max((o1,o2)->Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(after),new HashSet<Object>(before));
+        Assert.assertEquals(new HashSet<Object>(after),new HashSet<Object>(before));
+    */
+    before.add(group);
+    Comparator<? super GroupData> byId=(g1,g2)->Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(after,before);
   }
 
 }
