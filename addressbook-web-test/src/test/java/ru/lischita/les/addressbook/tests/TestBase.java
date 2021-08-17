1559a09bb88b6a560268ff1320a8f94d76b65e2c
lischita <87008989+lischita@users.noreply.github.com>
2021-08-17 11:48:28 +0300

    Подключение логов

--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/TestBase.java
@@ -1,11 +1,20 @@
 package ru.lischita.les.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.lischita.les.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -15,9 +24,20 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite (alwaysRun = true)
   public void tearDown() throws Exception
   {
     app.stop();
   }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object [] p)
+  {logger.info("Start method "+m.getName()+" with params "+ Arrays.asList(p));}
+
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("Stop method "+m.getName());}
+
+
 }
