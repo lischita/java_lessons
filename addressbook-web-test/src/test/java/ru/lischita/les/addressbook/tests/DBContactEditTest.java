908f167e75d80fe1b9264dfa779ad4a1d34fb025
lischita <87008989+lischita@users.noreply.github.com>
2021-08-22 12:00:40 +0300

    Домашнее Задание №15: Реализовать проверку данных, загружаемых из БД

--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/DBContactEditTest.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/DBContactEditTest.java
@@ -33,7 +33,11 @@ public class DBContactEditTest extends TestBase {
   {
     Contacts before=app.db().contacts();
     ContactData contactGroup=before.iterator().next();
-    ContactData contact =new ContactData().withId(contactGroup.getId()).withName("Яков").withLastname("Ронинсон");
+    ContactData contact =new ContactData().withId(contactGroup.getId()).withName("Яков").withMiddlename("Тестович").withLastname("Ронинсон")
+            .withNickname("Тестер").withTitle("Тестировщик").withCompany("Тест-Комплект").withAddress("г.Москва Тестовая дом 13")
+            .withHomephone("8-495-123-56-78").withMobilephone("8-976-456-67-87").withEmail("test@test_complect.ru")
+            .withBday("11").withBmonth("May").withByear("1980").withGroup("test1").withHomeaddress("г. Москва, ул. Тестиррования  дом 13")
+            .withWorkphone("495-123-45-67");
     app.goTo().HomePage();
     Assert.assertEquals(app.contact().all().size(), before.size() );
     Contacts after=app.db().contacts();
