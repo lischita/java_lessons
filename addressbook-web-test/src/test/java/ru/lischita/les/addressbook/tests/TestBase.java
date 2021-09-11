08537c9053659c0e00a9fa399ac29029d48aab48
lischita <87008989+lischita@users.noreply.github.com>
2021-09-11 09:24:34 +0300

    ddddd

--- addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/lischita/les/addressbook/tests/TestBase.java
@@ -24,7 +24,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception
