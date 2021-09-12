78d279514325954b95808e3f25ff84639d138b39
lischita <87008989+lischita@users.noreply.github.com>
2021-09-12 23:29:59 +0300

    Проект ДЗ21_2

--- jenkins-sample/src/test/java/ru/lischita/les/jenkins/tests/ContactDeleteTest.java
+++ jenkins-sample/src/test/java/ru/lischita/les/jenkins/tests/ContactDeleteTest.java
@@ -13,16 +13,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactDeleteTest extends TestBase{
 
-  @BeforeMethod
-  public void ensurePreconditions(){
-    if(app.contact().all().size()==0)
-    {
-      app.contact().crate(new ContactData().withName("Дмитрий").withMiddlename("Тестович").withLastname("Петрович").withNickname("Тестер").withTitle("Тестировщик").withCompany("Тест-Комплект").withAddress("г.Москва Тестовая дом 13").withHomephone("8-495-123-56-78").withMobilephone("8-976-456-67-87").withEmail("test@test_complect.ru").withBday("11").withBmonth("May").withByear("1980")
-              //.withGroup("test1") убрал перед выподнением ДЗ 16
-              .withHomeaddress("г. Москва, ул. Тестиррования  дом 13").withWorkphone("495-123-45-67"));
-    }
-  }
-
   @Test
   public void testContactDelete()
   {
